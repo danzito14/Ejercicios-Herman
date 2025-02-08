9c87a4070e035eaa49499404a4b7c1a58d31d52a
Uriel Alonso <urielalonsocaro@outlook.com>
2025-02-08 00:06:37 -0700

    Script del DNS Windows Server terminado

---
+++ UACD_DNS_Windows.ps1
@@ -0,0 +1,14 @@
+﻿#Primero instalamos el servicio de DNS
+Install-WindowsFeature -Name DNS -IncludeManagementTools
+
+#reiniciamos el servicio
+Restart-Service
+
+#Añadimos al servidor dns nuestro dominio en este caso reprobados.com
+Add-DnsServerPrimaryZone -Name reprobados.com -ZonaFile reprobados.com.dns
+
+#Luego añadimos un nombre para que se pueda encontrar
+Add-DnsServerResourceRecordA -IPv4Address 192.168.1.86 -Name www -ZonaName reprobados.com
+
+#Aqui vemos que se creo corecctamente
+Get-DnsServerZone
