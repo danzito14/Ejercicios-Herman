901e5d1386399b9737debf8216ded1e7383ae814
Uriel Alonso <urielalonsocaro@outlook.com>
2025-02-10 20:23:24 -0700

    Se arreglo el DNS_Windows
    Se arreglo el problema que tenia el script DNS_Windows, que permitia el ping con www.reprobados.com pero no con reprobados.com

--- UACD_DNS_Windows.ps1
+++ UACD_DNS_Windows.ps1
@@ -2,13 +2,16 @@
 Install-WindowsFeature -Name DNS -IncludeManagementTools
 
 #reiniciamos el servicio
-Restart-Service
+Restart-Service -Name DNS
 
 #Añadimos al servidor dns nuestro dominio en este caso reprobados.com
 Add-DnsServerPrimaryZone -Name reprobados.com -ZonaFile reprobados.com.dns
 
 #Luego añadimos un nombre para que se pueda encontrar
 Add-DnsServerResourceRecordA -IPv4Address 192.168.1.86 -Name www -ZonaName reprobados.com
+Add-DnsServerResourceRecordA -IPv4Address 192.168.1.86 -Name "@" -ZoneName reprobados.com
+
+Restart-Service -Name DNS
 
 #Aqui vemos que se creo corecctamente
 Get-DnsServerZone
