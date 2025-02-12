02a627020985bf3dd7bcfe5b58b2657069857155
Uriel Alonso <122856402+danzito14@users.noreply.github.com>
2025-02-12 22:31:58 +0100

    si

---
+++ UACD_DHCP_Windows.ps1
@@ -0,0 +1,22 @@
+﻿#Comandos para instalar el servicio y reiniciarlo
+Install-WindowsFeature -Name DHCP -IncludeManagementTools
+
+#Instalamos RSAT AD PowerShell, que es Remote Server Administration Tools, algunas veces no viene instalado por defecto
+Install-WindowsFeature -Name "RSAT-AD-PowerShell" -IncludeAllSubFeature
+
+#Instalamos las herramientas para crear un dominio
+Install-WindowsFeature -Name AD-Domain-Services -IncludeAllSubFeature
+Install-ADDForest -DomainName "midominio.com"
+
+Get-ADDomain
+
+Add-DhcpServerInDC -DnsName WIN-9LSTNUINFQG.midominio.local -IPAddress 192.168.1.8
+
+Add-DhcpServerv4Scope
+
+Get-DhcpServerv4Scope
+
+Set-DhcpServerv4optionValue -ScopeId 192.168.1.0 -DnsServer 192.168.1.86 -DnsDomain midominio.local -Router 192.168.1.8
+
+#Para ver las IP que se han dado
+Get-DhcpServerv4Lease
