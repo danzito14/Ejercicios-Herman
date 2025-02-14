bc6fdf7b83a9e9f11bab0538013f371eb4092bd8
Uriel Alonso <122856402+danzito14@users.noreply.github.com>
2025-02-14 06:12:06 +0100

    cambios finales

--- UACD_DHCP_Windows.ps1
+++ UACD_DHCP_Windows.ps1
@@ -1,44 +1,14 @@
 ﻿#Comandos para instalar el servicio y reiniciarlo
 Install-WindowsFeature -Name DHCP -IncludeManagementTools
 
-try{
- $pdc = (Get-ADDomain).PDCEmulator
+Add-DhcpServerInDC -IPAddress 192.168.1.86
+#COmando para añadir un rango, pedira ip inicial, fina, mascara de red etc
+Add-DhcpServerv4Scope
+Get-DchpServerv4ScopeS
 
-     Add-DhcpServerInDC -DnsName WIN-9LSTNUINFQG.midominio.local -IPAddress 192.168.1.86
+#aqui acompleta los datos faltantes
+et-DhcpServerv4optionValue -ScopeId 192.168.1.0 -DnsServer 192.168.1.86
 
-    Add-DhcpServerv4Scope
-
-    Get-DhcpServerv4Scope
-
-    Set-DhcpServerv4optionValue -ScopeId 192.168.1.0 -DnsServer 192.168.1.86 -DnsDomain midominio.local -Router 192.168.1.86
-
-
-}catch{
-    Write-Output "Ha surgido un error, su servidor no cuenta con las herramientas \nnecesarias para ver y o crear su propio dominio"
-    Write-Output "Desea instalar y crear su propio dominio, al final de la instalacion se reiniciara la maquina sola"
-    confirm = $confirmacion
-
-    if ($confirmacion = 'si' ){
-
-        #Instalamos RSAT AD PowerShell, que es Remote Server Administration Tools, algunas veces no viene instalado por defecto
-        Install-WindowsFeature -Name "RSAT-AD-PowerShell" -IncludeAllSubFeature
-
-        #Instalamos las herramientas para crear un dominio
-        Install-WindowsFeature -Name AD-Domain-Services -IncludeAllSubFeature
-
-        Write-Output "Ingrese un nombre de dominio para su servidor"
-        Install-ADDForest -DomainName "midominio.com"
-    } elseif ($confirmacion = 'no'){
-        Write-Output "Usted a decido no crear un dominio"
-        Write-Output "Se seguira la creacion del servidor DHCP sin un dominio"
-
-        Add-DhcpServerInDC -IPAddress 192.168.1.86
-        Add-DhcpServerv4Scope
-        Get-DchpServerv4Scope
-        Set-DhcpServerv4optionValue -ScopeId 192.168.1.0 -DnsServer 192.168.1.86
-    }
-
-}
 
 #Para ver las IP que se han dado
 Get-DhcpServerv4Lease
